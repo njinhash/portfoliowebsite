458470ca6523726eba4efd4e1e19c1e7493c914e
njinhash <d4niel4cebo@gmail.com>
2024-07-08 00:21:28 -0700

    update 7/8

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-function ProjectCard({ src, link, h3, p }) {
+
+
+function ProjectCard({ src, link, h3, p, githubLink}) {
   return (
-    <a href={link} target="_blank">
+    <div>
+      <a href={link} rel="noreferrer" target="_blank">
       <img className="hover" src={src} alt={`${h3} logo`} />
       <h3>{h3}</h3>
       <p>{p}</p>
     </a>
+      <br />
+      <a href={githubLink} rel="noreferrer" target="_blank"><button>Github</button></a>
+    </div>
   );
 }
 
--- src/sections/2Projects/Projects.jsx
+++ src/sections/2Projects/Projects.jsx
@@ -5,47 +5,42 @@ import hipsster from '../../assets/hipsster.png';
 import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
 
-const projects = [
-  {
-    src: viberr,
-    link: "https://github.com/Ade-mir/company-landing-page-2",
-    title: "Viberr",
-    description: "Streaming App"
-  },
-  {
-    src: freshBurger,
-    link: "https://github.com/Ade-mir/company-landing-page-2",
-    title: "Fresh Burger",
-    description: "Hamburger Restaurant"
-  },
-  {
-    src: hipsster,
-    link: "https://github.com/Ade-mir/company-landing-page-2",
-    title: "Hipsster",
-    description: "Glasses Shop"
-  },
-  {
-    src: fitLift,
-    link: "https://github.com/Ade-mir/company-landing-page-2",
-    title: "FitLift",
-    description: "Fitness App"
-  }
-];
+
+
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        {projects.map((project, index) => (
-          <ProjectCard
-            key={index}
-            src={project.src}
-            link={project.link}
-            h3={project.title}
-            p={project.description}
-          />
-        ))}
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Viberr"
+          p="Streaming App"
+          githubLink="https://www.youtube.com/"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Fresh Burger"
+          p="Hamburger Restaurant"
+          githubLink="https://www.netflix.com/"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Hipsster"
+          p="Glasses Shop"
+          githubLink="https://www.google.com/"
+        />
+        <ProjectCard
+          src={fitLift}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="FitLift"
+          p="Fitness App"
+          githubLink="https://www.facebook.com/"
+        />
       </div>
     </section>
   );
